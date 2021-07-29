dfe449f51f97635fc7cc05ad625c275f6b0c916e
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-07-29 21:40:38 +0100

    update template markup

--- single.php
+++ single.php
@@ -11,30 +11,33 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+		<div class="blog-col-1">
+			<?php
+			while ( have_posts() ) :
+				the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+				get_template_part( 'template-parts/content', get_post_type() );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+				the_post_navigation(
+					array(
+						'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Prev:', 'legal-aspire-solicitors' ) . '</span> <span class="nav-title">%title</span>',
+						'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'legal-aspire-solicitors' ) . '</span> <span class="nav-title">%title</span>',
+					)
+				);
 
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'legal-aspire-solicitors' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'legal-aspire-solicitors' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			endwhile; // End of the loop.
+			?>
+		</div>
 
-		endwhile; // End of the loop.
-		?>
-
+		<div class="blog-col-2">
+			<?php get_sidebar(); ?>
+		</div>
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
 get_footer();
