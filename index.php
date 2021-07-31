4517865bf1af255d83ba69bfa817b1198346b635
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-07-31 18:17:23 +0100

    update news page markup

--- index.php
+++ index.php
@@ -14,43 +14,38 @@
 
 get_header();
 ?>
-
 	<main id="primary" class="site-main">
+	<section class="news-pg">
+	
+	<div class="news-pg__banner-image-background">
+        <div class="news-pg__banner-content">
+            <h1 class="news-pg__banner-title">News</h1>
+        </div>
+    </div>
 
+	<div class="news-pg__container">
 		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
+		if ( have_posts() ) {
+			while(have_posts()): the_post();
 		?>
 
-	</main><!-- #main -->
+	
+				<div class="news-pg__container-item">
+					<div class="news-pg__thumbnail"><?php legal_aspire_solicitors_post_thumbnail(); ?></div>
+					<p class="news-pg__date"><?php legal_aspire_solicitors_posted_on(); ?></p>
+					<h2 class="news-pg__title"><?php the_title(); ?></h2>
+					<div class="news-pg__excerpt"><?php the_excerpt(); ?></div>
+					<a class="news-pg__permalink" href="<?php the_permalink(); ?>">Read more</a>
+				</div>
+		
+
+		<?php endwhile; } ?>
+		</div>
+
+		<nav class="news-pg__pagination"><?php echo paginate_links(); ?></nav>
+
+		</section>
+	</main>
 
 <?php
 get_sidebar();
