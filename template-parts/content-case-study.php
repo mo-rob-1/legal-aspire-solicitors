1eeee910b161b044b8ec1d6f45366fa8dcbf04d1
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-08-10 22:29:27 +0100

    update case study markup

--- template-parts/content-case-study.php
+++ template-parts/content-case-study.php
@@ -14,9 +14,9 @@
 		<header class="entry-header">
 			<?php
 			if ( is_singular() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
+				the_title( '<h1 class="entry-title-case-study">', '</h1>' );
 			else :
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				the_title( '<h2 class="entry-title-case-study"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			endif;
 
 			if ( 'post' === get_post_type() ) :
@@ -30,27 +30,47 @@
 		</div>
 	</div>
 
-	<div class="entry-content">
+	<div class="entry-content-case-study">
 
     <div class="case-study-review-container">
         <span class="case-study-span">Case</span>
         <h2 class="case-study-overview-heading">Overview</h2>
         <p class="case-study-overview"><?php the_field('case_study_overview'); ?></p>
-        <div>
-            <h3 class="case-study-solicitor-heading">Solicitor</h3>
-            <p class="case-study-solicitor"><?php the_field('case_study_solicitor'); ?></p>
+        <div class="case-study-overview-icon-wrapper">
+            <div class="case-study-icon-wrapper">
+				<img class="case-study-icon" src="http://legal-aspire-solicitors.local/wp-content/uploads/2021/08/user.png" alt="Icon" />
+			</div>
+            <div class="case-study-text-info-wrapper">
+                <h3 class="case-study-solicitor-heading">Solicitor:</h3>
+                <p class="case-study-solicitor"><?php the_field('case_study_solicitor'); ?></p>
+            </div>
         </div>
-        <div>
-            <h3 class="case-study-category-heading">Category</h3>
-            <p class="case-study-category"><?php the_field('case_study_category'); ?></p>
+        <div class="case-study-overview-icon-wrapper">
+            <div class="case-study-icon-wrapper">
+				<img class="case-study-icon" src="http://legal-aspire-solicitors.local/wp-content/uploads/2021/08/list.png" alt="Icon" />
+			</div>
+            <div class="case-study-text-info-wrapper">
+                <h3 class="case-study-category-heading">Category:</h3>
+                <p class="case-study-category"><?php the_field('case_study_category'); ?></p>
+            </div>
         </div>
-        <div>
-            <h3 class="case-study-time-heading">Time</h3>
-            <p class="case-study-time"><?php the_field('case_study_time'); ?></p>
+        <div class="case-study-overview-icon-wrapper">
+            <div class="case-study-icon-wrapper">
+				<img class="case-study-icon" src="http://legal-aspire-solicitors.local/wp-content/uploads/2021/08/time-left.png" alt="Icon" />
+			</div>
+            <div class="case-study-text-info-wrapper">
+                <h3 class="case-study-time-heading">Time:</h3>
+                <p class="case-study-time"><?php the_field('case_study_time'); ?></p>
+            </div>
         </div>
-        <div>
-            <h3 class="case-study-result-heading">Result</h3>
-            <p class="case-study-result"><?php the_field('case_study_result'); ?></p>
+        <div class="case-study-overview-icon-wrapper">
+            <div class="case-study-icon-wrapper">
+				<img class="case-study-icon" src="http://legal-aspire-solicitors.local/wp-content/uploads/2021/08/money.png" alt="Icon" />
+			</div>
+            <div class="case-study-text-info-wrapper">
+                <h3 class="case-study-result-heading">Result:</h3>
+                <p class="case-study-result"><?php the_field('case_study_result'); ?></p>
+            </div>
         </div>
         <div class="case-study-overview-img"><?php the_field('case_study_image') ?></div>
     </div>
@@ -59,30 +79,32 @@
         <?php the_content(); ?>
     </div>
 
-    <?php
-        $args = array(
-            'post_type' => 'case-study',
-            'posts_per_page' => 3
-        );
+    <div class="case-study-more-cards-container">
+        <?php
+            $args = array(
+                'post_type' => 'case-study',
+                'posts_per_page' => 3
+            );
 
-        $post_query = new WP_Query($args);
+            $post_query = new WP_Query($args);
 
-        if($post_query->have_posts() ) {
-            while($post_query->have_posts() ) {
-                $post_query->the_post();
-                ?>
-                    <div class="case-study-card">
-                        <div class="case-study-card-img-wrapper"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></div>
-                            <div class="case-study-card-text-wrapper">
-                                <h2 class="case-study-card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                                <p class="case-study-card-excerpt"><?php echo wp_trim_words(get_the_excerpt(), 17); ?></p>
-                                <p class="case-study-card-link-wrapper"><a class="case-study-card-link" href="<?php the_permalink(); ?>">Read more</a></p>
-                            </div>
-                    </div>
-                <?php
-                    } wp_reset_postdata();
-                }
+            if($post_query->have_posts() ) {
+                while($post_query->have_posts() ) {
+                    $post_query->the_post();
+                    ?>
+                        <div class="case-study-card">
+                            <div class="case-study-card-img-wrapper"><a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></div>
+                                <div class="case-study-card-text-wrapper">
+                                    <h2 class="case-study-card-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                    <p class="case-study-card-excerpt"><?php echo wp_trim_words(get_the_excerpt(), 17); ?></p>
+                                    <p class="case-study-card-link-wrapper"><a class="case-study-card-link" href="<?php the_permalink(); ?>">Read more</a></p>
+                                </div>
+                        </div>
+                    <?php
+                        } wp_reset_postdata();
+                    }
             ?>
+        </div>
 
 	</div><!-- .entry-content -->
 
