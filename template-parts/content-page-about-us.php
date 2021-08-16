bc29eec87cee13c5d59202a15b6a432590ff1c1f
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-08-16 21:10:46 +0100

    add class names

--- template-parts/content-page-about-us.php
+++ template-parts/content-page-about-us.php
@@ -16,35 +16,34 @@
 		</div>
 	</div>
 
-    <!-- <div class="about-us-pg__container"> -->
         <div class="about-us-pg__item-one-wrapper">
             <div class="about-us-pg__col-1">
-                <h5>Who We Are</h5>
-                <h2>Your partner for legal</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit magni accusamus sapiente in fugiat cum, tempore sunt aspernatur iure necessitatibus ab, 
+                <h5 class="about-us-pg__intro-title">Who We Are</h5>
+                <h2 class="about-us-pg__col-1-title">Your partner for legal advice</h2>
+                <p class="about-us-pg__col-1-desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit magni accusamus sapiente in fugiat cum, tempore sunt aspernatur iure necessitatibus ab, 
                 cumque numquam obcaecati itaque fugit nostrum, asperiores placeat doloremque facilis ut animi culpa. Esse alias, eius quaerat explicabo corrupti omnis mollitia odit 
                 commodi laboriosam maiores quis facere deleniti libero quia voluptate non quam blanditiis ab dignissimos. Esse, minus ullam.</p>
             </div>
 
             <div class="about-us-pg__col-2">
-                <img class="experience__img" src="http://legal-aspire-solicitors.local/wp-content/uploads/2021/07/solicitors-coversation.jpg" alt="Image" />
+                <img class="about-us-pg__img" src="http://legal-aspire-solicitors.local/wp-content/uploads/2021/07/solicitors-coversation.jpg" alt="Image" />
             </div>
         </div>
 
         <div class="about-us-pg__item-two-wrapper">
             <div class="about-us-pg__item-two-col-1">
-                <h3>01 Legal Advisors</h3>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis aspernatur a consequuntur tenetur. 
+                <h3 class="about-us-pg__item-two-title">01 Legal Advisors</h3>
+                <p class="about-us-pg__item-two-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis aspernatur a consequuntur tenetur. 
                 Necessitatibus aliquid totam in distinctio hic deserunt</p>
             </div>
             <div class="about-us-pg__item-two-col-2">
-                <h3>02 Trusted Experience</h3>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis aspernatur a consequuntur tenetur. 
+                <h3 class="about-us-pg__item-two-title">02 Trusted Experience</h3>
+                <p class="about-us-pg__item-two-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis aspernatur a consequuntur tenetur. 
                 Necessitatibus aliquid totam in distinctio hic deserunt</p>
             </div>
             <div class="about-us-pg__item-two-col-3">
-                <h3>03 Responsiveness</h3>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis aspernatur a consequuntur tenetur. 
+                <h3 class="about-us-pg__item-two-title">03 Responsiveness</h3>
+                <p class="about-us-pg__item-two-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis aspernatur a consequuntur tenetur. 
                 Necessitatibus aliquid totam in distinctio hic deserunt</p>
             </div>
         </div>
@@ -58,59 +57,61 @@
 
             <ul class="js-filter uk-child-width-1-2 uk-child-width-1-3@m uk-text-center" uk-grid>
                 <li data-color="white">
-                    <div class="uk-card uk-card-default uk-card-body card-box-shadow-none">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</div>
+                    <div class="uk-card uk-card-default uk-card-body card-box-shadow-none about-us-pg__item-filter-desc">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</div>
                 </li>
                 <li data-color="blue">
-                    <div class="uk-card uk-card-body card-box-shadow-none">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis aspernatur a consequuntur tenetur. 
+                    <div class="uk-card uk-card-body card-box-shadow-none about-us-pg__item-filter-desc">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis aspernatur a consequuntur tenetur. 
                     Necessitatibus aliquid totam in distinctio hic deserunt.</div>
                 </li>
                 <li data-color="black">
-                    <div class="uk-card uk-card-default uk-card-body card-box-shadow-none">Ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</div>
+                    <div class="uk-card uk-card-default uk-card-body card-box-shadow-none about-us-pg__item-filter-desc">Ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</div>
                 </li>
             </ul>
         </div>
 
+    <div class="timeline-wrapper">  
     <div class="timeline">
         <div class="container left">
             <div class="content">
-            <h2>2017</h2>
-            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            <h2 class="about-us-pg__timeline-heading">2017</h2>
+            <p class="about-us-pg__timeline-desc">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
             </div>
         </div>
         <div class="container right">
             <div class="content">
-            <h2>2016</h2>
-            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            <h2 class="about-us-pg__timeline-heading">2016</h2>
+            <p class="about-us-pg__timeline-desc">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
             </div>
         </div>
         <div class="container left">
             <div class="content">
-            <h2>2015</h2>
-            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            <h2 class="about-us-pg__timeline-heading">2015</h2>
+            <p class="about-us-pg__timeline-desc">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
             </div>
         </div>
         <div class="container right">
             <div class="content">
-            <h2>2012</h2>
-            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            <h2 class="about-us-pg__timeline-heading">2012</h2>
+            <p class="about-us-pg__timeline-desc">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
             </div>
         </div>
         <div class="container left">
             <div class="content">
-            <h2>2011</h2>
-            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            <h2 class="about-us-pg__timeline-heading">2011</h2>
+            <p class="about-us-pg__timeline-desc">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
             </div>
         </div>
         <div class="container right">
             <div class="content">
-            <h2>2007</h2>
-            <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+            <h2 class="about-us-pg__timeline-heading">2007</h2>
+            <p class="about-us-pg__timeline-desc">Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
             </div>
         </div>
     </div>
+    </div> 
 
     <div class="about-us-pg__team">
-        <h2>Meet Our Legal Experts</h2>
+        <h2 class="about-us-pg__team-title">Meet Our Legal Experts</h2>
         <?php
             $args = array(
                 'post_type' => 'our-team',
@@ -136,6 +137,8 @@
         ?>
         <a class="about-us-pg__team-members-link" href="<?php echo site_url('/our-team') ?>">View All</a>
 
+        </div>
+
         <section class="contact services-pg-contact">
 			<div class="contact__container">
 				<h2 class="contact__heading">Get a Free Counsultation for Your Case.</h2>
@@ -144,7 +147,7 @@
 				</div>
 			</div>
 		</section>
-    </div>
-
+    
+ 
     </div>
 </section><!-- #post-<?php the_ID(); ?> -->
