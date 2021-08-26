37d46b6ea745b87cc093ad6577d5f3016a412989
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-08-26 22:14:35 +0100

    remove comments

--- header.php
+++ header.php
@@ -30,26 +30,9 @@
             </li>
             <li class="uk-parent">
                 <a href="<?php echo site_url('/services') ?>">Services</a>
-                <!-- <ul class="uk-nav-sub">
-                    <li><a href="<?php echo site_url('/services/personal-injury') ?>">Personal Injury</a></li>
-                    <li><a href="<?php echo site_url('/services/medical-negligence') ?>">Medical Negligence</a></li>
-                    <li><a href="<?php echo site_url('/services/employment') ?>">Employment</a></li>
-                    <li><a href="<?php echo site_url('/services/property') ?>">Property</a></li>
-                    <li><a href="<?php echo site_url('/services/family') ?>">Family</a></li>
-                    <li><a href="<?php echo site_url('/services/immigration') ?>">Immigration</a></li>
-                    <li><a href="<?php echo site_url('/services/pensions') ?>">Pensions</a></li>
-                </ul> -->
             </li>
             <li class="uk-parent">
                 <a href="<?php echo site_url('/sectors') ?>">Sectors</a>
-                <!-- <ul class="uk-nav-sub">
-                    <li><a href="<?php echo site_url('/sectors/real-estate-law') ?>">Real Estate Law</a></li>
-                    <li><a href="<?php echo site_url('/sectors/recruitment-law') ?>">Recruitment Law</a></li>
-                    <li><a href="<?php echo site_url('/sectors/healthcare-law') ?>">Healthcare Law</a></li>
-                    <li><a href="<?php echo site_url('/sectors/technology-law') ?>">Technology Law</a></li>
-                    <li><a href="<?php echo site_url('/sectors/insurance-law') ?>">Insurance Law</a></li>
-                    <li><a href="<?php echo site_url('/sectors/retail-law') ?>">Retail Law</a></li>
-                </ul> -->
             </li>
             <li class="uk-active"><a href="<?php echo site_url('/news') ?>">News</a></li>
             <li class="uk-active"><a href="<?php echo site_url('/contact') ?>">Contact</a></li>
@@ -60,22 +43,11 @@
 
 		<div class="header__wrapper-mobile">
         <div class="header__logo-wrapper">
-          <!-- <a class="header__logo-link" href="#"><img class="header__logo" src="images/logo.svg" alt="Legal Aspire Solicitors" /></a> -->
 		      <a class="header__logo-link" href="<?php echo home_url(); ?>">Legal Aspire Solicitors</a>
         </div>
 
-        <!-- <div class="header__hamburger-wrapper">
-          <button id="toggle_btn" class="hamburger hamburger--squeeze js-hamburger menu-icon" type="button">
-              <span class="hamburger-box">
-              <span class="hamburger-inner"></span>
-              </span>
-          </button>
-        </div> -->
-
         <button class="uk-button uk-button-default uk-margin-small-right hamburger-icon-uikit" type="button" uk-toggle="target: #offcanvas-nav-primary"><i class="fas fa-bars"></i></button>
       </div>
-
-      <!-- <div class="clk"><a href="javascript:void"><i class="fa fa-plus-circle"></i>some text</a><div> -->
 
         <nav class="header__nav show-nav">
           <ul class="header__nav-list">
