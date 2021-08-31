d51d49013ce08a18de8a1ca741c07bb5068a35e4
Mo <31892724+mo-rob-1@users.noreply.github.com>
2021-08-31 21:04:16 +0100

    update markup

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -9,27 +9,18 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header-search">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+<article class="search-container" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header-search">
+			<?php the_title( sprintf( '<h2 class="entry-title-search"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
-			legal_aspire_solicitors_posted_on();
-			legal_aspire_solicitors_posted_by();
-			?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+			<?php if ( 'post' === get_post_type() ) : ?>
+			<?php endif; ?>
+		</header><!-- .entry-header -->
 
-	<?php legal_aspire_solicitors_post_thumbnail(); ?>
+		<?php legal_aspire_solicitors_post_thumbnail(); ?>
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+		<div class="entry-summary-search">
+			<?php the_excerpt(); ?>
+		</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php legal_aspire_solicitors_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
